5139b50ba37a658eea1f3ee9320046dbe9ba2d51
Partha <partha.beas@gmail.com>
2023-05-24 12:46:44 +0530

    test

--- routes/api.php
+++ routes/api.php
@@ -73,7 +73,7 @@ use App\Http\Controllers\Api\Modules\Admin\LedgerController;
 |
 */
 // Route::post('login', [UserController::class, 'login']);
-
+//check
 // ----------------------------------- register -----------------------------------------
 
 Route::post('/register', [UserController::class,'store']);
